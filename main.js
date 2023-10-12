107a5f4ad81feab7bb6a09eeed4e9b94a45e2a0f
Yuri Moreira <yurikauanim@gmail.coom>
2023-10-12 00:58:47 -0300

    atualização

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 function toggleElementVisibility(elementId) {
     const element = document.getElementById(elementId);
     if (element.style.display === 'none' || element.style.display === '') {
-      element.style.display = 'block';
+      element.style.display = 'flex';
     } else {
       element.style.display = 'none';
     }
@@ -12,4 +12,24 @@ function toggleElementVisibility(elementId) {
   const toggleButton = document.getElementById('toggle-button');
   toggleButton.addEventListener('click', function () {
     toggleElementVisibility('element-to-toggle');
+  });
+
+  const toggleButton2 = document.getElementById('toggle-button2');
+  toggleButton2.addEventListener('click', function () {
+    toggleElementVisibility('element-to-toggle2');
+  });
+
+  const toggleButton3 = document.getElementById('toggle-button3');
+  toggleButton3.addEventListener('click', function () {
+    toggleElementVisibility('element-to-toggle3');
+  });
+
+  const toggleButton4 = document.getElementById('toggle-button4');
+  toggleButton4.addEventListener('click', function () {
+    toggleElementVisibility('element-to-toggle4');
+  });
+
+  const toggleButton5 = document.getElementById('toggle-button5');
+  toggleButton5.addEventListener('click', function () {
+    toggleElementVisibility('element-to-toggle5');
   });
